f653535581bdf1b00b118a6eb8451ec2ddda5951
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-02-02 23:08:40 +0300

    1.2.3. Наследование - 3. Вызов конструктора родителя super(...)

---
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class Engineer extends Profession {
+    private int experience;
+
+    public Engineer(int experience, boolean degree) {
+        this.experience = experience;
+    }
+}
