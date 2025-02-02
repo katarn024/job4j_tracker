caf338c571cdbd388aa74d9285cfb6d06dbfcc94
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-02-02 23:09:36 +0300

    1.2.3. Наследование - 3. Вызов конструктора родителя super(...) с исправлением в классе Engineer

--- src/main/java/ru/job4j/inheritance/Engineer.java
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -4,6 +4,7 @@ public class Engineer extends Profession {
     private int experience;
 
     public Engineer(int experience, boolean degree) {
+        super(degree);
         this.experience = experience;
     }
 }
