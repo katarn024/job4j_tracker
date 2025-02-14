96d130097ff592aeddde2a4a60e169933f95dba9
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-02-15 00:21:14 +0300

    1.2.4. Инкапсуляция - Небольшие исправления в классе Person

--- src/main/java/ru/job4j/record/Person.java
+++ src/main/java/ru/job4j/record/Person.java
@@ -7,7 +7,6 @@ public class Person {
     private final String name;
     private final int age;
 
-
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
@@ -16,6 +15,7 @@ public class Person {
     public String getName() {
         return name;
     }
+
     public int getAge() {
         return age;
     }
