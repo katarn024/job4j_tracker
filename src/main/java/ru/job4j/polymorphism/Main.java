3bdc0605026381b3270ec125c8ebb701e3a05054
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-02-20 17:20:09 +0300

    1.2.5. Полиморфизм - 4.0. Что такое Interface (промежуточный)

---
+++ src/main/java/ru/job4j/polymorphism/Main.java
@@ -0,0 +1,11 @@
+package ru.job4j.polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle townCar = new TownCar();
+        townCar.changeGear();
+        townCar.accelerate();
+        townCar.steer();
+        townCar.brake();
+    }
+}
