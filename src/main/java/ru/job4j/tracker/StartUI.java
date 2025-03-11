8cc009e986a67cf21f6ee2dacdb6b4c999400faa
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-03-11 22:31:07 +0300

    1.2.5. Полиморфизм - 6.1. Разрыв зависимости StartUI от Scanner

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,20 +1,18 @@
 package ru.job4j.tracker;
 
-import java.util.Scanner;
-
 public class StartUI {
 
-    public void init(Scanner scanner, Tracker tracker) {
+    public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             showMenu();
             System.out.print("Выбрать: ");
-            int select = Integer.parseInt(scanner.nextLine());
+            int select = input.askInt("Enter id: ");
             System.out.println("пользователь выбрал: " + select);
             if (select == 0) {
                 System.out.println("=== Создание новой заявки ===");
                 System.out.print("Пожалуйста, введите имя: ");
-                String name = scanner.nextLine();
+                String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
@@ -31,9 +29,9 @@ public class StartUI {
             } else if (select == 2) {
                 System.out.println("=== Редактирование заявки ===");
                 System.out.print("Введите id: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = input.askInt("Enter id: ");
                 System.out.print("Введите имя: ");
-                String name = scanner.nextLine();
+                String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 if (tracker.replace(id, item)) {
                     System.out.println("Заявка заменена успешно");
@@ -43,14 +41,14 @@ public class StartUI {
             } else if (select == 3) {
                 System.out.println("=== Удаление заявки ===");
                 System.out.print("Введите id: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = input.askInt("Enter id: ");
                 Item item = tracker.findById(id);
                 tracker.delete(id);
                 System.out.println(item != null ? "Заявка удалена успешно" : "Ошибка удаления заявки");
             } else if (select == 4) {
                 System.out.println("=== вывод заявки по id ===");
                 System.out.print("ВВведите id: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = input.askInt("Enter id: ");
                 Item item = tracker.findById(id);
                 /* При проверке найденного объекта на null не стоит использовать конструкцию:
                 if (item.equals(null)) {};
@@ -66,7 +64,7 @@ public class StartUI {
             } else if (select == 5) {
                 System.out.println("=== Вывод заявок по имени ===");
                 System.out.print("Введите имя: ");
-                String name = scanner.nextLine();
+                String name = input.askStr("Enter name: ");
                 Item[] items = tracker.findByName(name);
                 if (items.length > 0) {
                     for (Item item : items) {
@@ -97,10 +95,10 @@ public class StartUI {
     public static void main(String[] args) {
         /* Item item = new Item(1, "Test");
         System.out.println(item);*/
-        Scanner scanner = new Scanner(System.in);
+        Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         /* Мы создаем объект класса StartUI и сразу на этом объекте вызываем метод init().
          * Поскольку дальше этот объект мы не будем переиспользовать, то мы не записываем созданный объект в переменную.  */
-        new StartUI().init(scanner, tracker);
+        new StartUI().init(input, tracker);
     }
 }
