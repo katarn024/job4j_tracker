f3c1f1f08cb380699304866dbd13db0ed906b2cb
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-03-15 09:31:52 +0300

    1.2.5. Полиморфизм - 9.1. Тестирование. Подготовка данных.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -32,4 +32,18 @@ class StartUITest {
         Item edited = tracker.findById(item.getId());
         assertThat(edited.getName()).isEqualTo("edited item");
     }
-}ц
+
+    @Test
+    void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.deleteItem(new MockInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited).isEqualTo(null);
+    }
+}
