2f0e3b3844ec71003bd2076501d33fc8dc9b0715
katarn024 <52570486+katarn024@users.noreply.github.com>
2025-05-24 23:00:37 +0300

    1.3.1. Коллекция List, ArrayList  - 9. Изменить программу Tracker из 2-го модуля (дополнение)

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Tracker {
     private final ArrayList<Item> items = new ArrayList<>();
     private int ids = 1;
-    private int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);
@@ -19,7 +18,6 @@ public class Tracker {
 
     public ArrayList<Item> findByName(String key) {
         ArrayList<Item> result = new ArrayList<>();
-        int number = 0;
         for (Item item : items) {
             if (item.getName().equals(key)) {
                 result.add(item);
@@ -35,7 +33,7 @@ public class Tracker {
 
     public int indexOf(int id) {
         int result = -1;
-        for (int index = 0; index < size; index++) {
+        for (int index = 0; index < items.size(); index++) {
             if (items.get(index).getId() == id) {
                 result = index;
                 break;
@@ -56,12 +54,8 @@ public class Tracker {
 
     public void delete(int id) {
         int index = indexOf(id);
-        boolean result = index != -1;
-        if (result) {
-            items.set(index, null);
-            System.arraycopy(items, index + 1, items, index, size - (index - 1));
-            items.set(size - 1, null);
-            size--;
+         if (index != -1) {
+            items.remove(index);
         }
     }
 }
